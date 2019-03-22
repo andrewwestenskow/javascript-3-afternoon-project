ffae24e955244dd0b55c14f33a000e914da08c12
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-03-22 15:28:33 -0600

    finished higher order functions

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,19 +1,15 @@
-var values = {
-  one: 'These',
-  two: ' are',
-  three: ' the',
-  four: ' property',
-  five: ' values.'
-} 
-
-for(var key in values) {
-  console.log(values[key])
-}
+const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
+{"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
+{"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
+{"price":48,"tax":0.13}];
+// Do not edit code above.
 
 /*
-  In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
+  Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-for(var key in values) {
-  console.log(key)
-}
+let orderTotals = orders.map(function(num){
+  return num.price + (num.price * num.tax)
+})
+
+console.log(orderTotals)
